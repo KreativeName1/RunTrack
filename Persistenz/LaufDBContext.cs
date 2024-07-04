1522fa048aea3de83af72ab6666b7316300768a5
Sascha Dierl <115576847+KreativeName1@users.noreply.github.com>
2024-07-04 10:49:08 +0200

    database

---
+++ Persistenz/LaufDBContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SQLite
+{
+    public class LaufDBContext : DbContext
+    {
+        private static string _dbPath = "Datenbanken/EigeneDatenbank.db";
+        public LaufDBContext()
+        : base(GetDbContextOptions())
+        {
+            Directory.CreateDirectory("Datenbanken");
+            Database.EnsureCreated();
+           
+        }
+        public LaufDBContext(string _path)
+       : base(GetDbContextOptions())
+        {
+            _dbPath = _path;
+            Database.EnsureCreated();
+        }
+
+
+        private static DbContextOptions GetDbContextOptions()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LaufDBContext>();
+            optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+            optionsBuilder.EnableSensitiveDataLogging();
+            return optionsBuilder.Options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Schule>()
+         .HasMany(s => s.Klassen)
+         .WithOne(k => k.Schule)
+         .HasForeignKey(k => k.SchuleId);
+
+            modelBuilder.Entity<Klasse>()
+                .HasMany(k => k.Schueler)
+                .WithOne(s => s.Klasse)
+                .HasForeignKey(s => s.KlasseId);
+
+            modelBuilder.Entity<Schueler>()
+                .HasMany(s => s.Runden)
+                .WithOne(r => r.Schueler)
+                .HasForeignKey(r => r.SchuelerId);
+        }
+
+
+        public DbSet<Klasse> Klassen { get; set; }
+        public DbSet<Schule> Schulen { get; set; }
+        public DbSet<Schueler> Schueler { get; set; }
+        public DbSet<Runde> Runden { get; set; }
+        public DbSet<RundenArt> RundenArten { get; set; }
+
+
+    }
+}
---
+++ Persistenz/MergedDBContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite
+{
+    public class MergedDBContext : DbContext
+    {
+        public MergedDBContext(string[] databases)
+        : base(GetDbContextOptions())
+        {
+            Database.EnsureCreated();
+
+            // get all data from the Runden table
+            foreach (string db_path in databases)
+            {
+                using (var db = new LaufDBContext(db_path))
+                {
+                    foreach (var runde in db.Runden)
+                    {
+                        runde.Id = 0;
+                        Schueler schueler;
+                        List<Schueler> schuelerList;
+                        using (var db2 = new LaufDBContext())
+                        {
+                            schuelerList = db2.Schueler.AsNoTracking().ToList();
+                        }
+
+                        runde.Schueler = schuelerList.FirstOrDefault(s => s.Id == runde.SchuelerId);
+                        this.Runden.Add(runde);
+                    }
+                    this.SaveChanges();
+                }
+            }
+        }
+
+        private static DbContextOptions GetDbContextOptions()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MergedDBContext>();
+            optionsBuilder.UseSqlite($"Data Source=internal.db");
+            optionsBuilder.EnableSensitiveDataLogging();
+            return optionsBuilder.Options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Schule>()
+         .HasMany(s => s.Klassen)
+         .WithOne(k => k.Schule)
+         .HasForeignKey(k => k.SchuleId);
+
+            modelBuilder.Entity<Klasse>()
+                .HasMany(k => k.Schueler)
+                .WithOne(s => s.Klasse)
+                .HasForeignKey(s => s.KlasseId);
+
+            modelBuilder.Entity<Schueler>()
+                .HasMany(s => s.Runden)
+                .WithOne(r => r.Schueler)
+                .HasForeignKey(r => r.SchuelerId);
+        }
+
+
+        public DbSet<Klasse> Klassen { get; set; }
+        public DbSet<Schule> Schulen { get; set; }
+        public DbSet<Schueler> Schueler { get; set; }
+        public DbSet<Runde> Runden { get; set; }
+        public DbSet<RundenArt> RundenArten { get; set; }
+
+
+    }
+}
