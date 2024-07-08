f443e999c80fc8500f0ba6030f87a80e79859f5a
Paul Fischer <fischerp@bswiesaude.onmicrosoft.com>
2024-07-08 12:41:51 +0200

    Loginfenster (MainWindow) übrearbeitet

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -5,128 +5,136 @@ using System.Windows.Media;
 
 namespace Klimalauf
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            string firstName = FirstNameTextBox.Text;
-            string lastName = LastNameTextBox.Text;
 
-            // Check if first name and last name are provided
-            if (ValidateInputs())
-            {
-                // Check admin password
-                string adminPassword = AdminPasswordBox.Password;
-                bool isAdmin = adminPassword == "test" && firstName == "admin" && lastName == "admin";
+      private void Window_Loaded(object sender, RoutedEventArgs e)
+      {
+         FirstNameTextBox.Foreground = new SolidColorBrush(Colors.Gray);
+         LastNameTextBox.Foreground = new SolidColorBrush(Colors.Gray);
+      }
 
-                Scanner scanner = new Scanner(firstName, lastName, isAdmin);
-                scanner.Show();
-                this.Close();
+         private void LoginButton_Click(object sender, RoutedEventArgs e)
+      {
+         string firstName = FirstNameTextBox.Text;
+         string lastName = LastNameTextBox.Text;
 
-                //if (isAdmin)
-                //{
-                //   // Open admin panel window
-                //   AdminScanner adminPanel = new AdminScanner(firstName, lastName);
-                //   adminPanel.Show();
-                //   this.Close();
-                //}
-                //else
-                //{
-                //   // Open admin panel window
-                //   Scanner panel = new Scanner(firstName, lastName);
-                //   panel.Show();
-                //   this.Close();
-                //}
-            }
-        }
+         // Check if first name and last name are provided
+         if (ValidateInputs())
+         {
+            // Check admin password
+            string adminPassword = AdminPasswordBox.Password;
+            bool isAdmin = adminPassword == "test" && firstName == "admin" && lastName == "admin";
 
-        private bool ValidateInputs()
-        {
-            bool isValid = true;
 
-            if (string.IsNullOrWhiteSpace(FirstNameTextBox.Text) || FirstNameTextBox.Text == "Max")
-            {
-                SetInvalidInputStyle(FirstNameTextBox);
-                isValid = false;
-            }
-            else
-            {
-                SetValidInputStyle(FirstNameTextBox);
-            }
+            Scanner scanner = new Scanner(firstName, lastName, isAdmin);
+            scanner.Show();
+            this.Close();
 
-            if (string.IsNullOrWhiteSpace(LastNameTextBox.Text) || LastNameTextBox.Text == "Mustermann")
-            {
-                SetInvalidInputStyle(LastNameTextBox);
-                isValid = false;
-            }
-            else
-            {
-                SetValidInputStyle(LastNameTextBox);
-            }
+            //if (isAdmin)
+            //{
+            //   // Open admin panel window
+            //   AdminScanner adminPanel = new AdminScanner(firstName, lastName);
+            //   adminPanel.Show();
+            //   this.Close();
+            //}
+            //else
+            //{
+            //   // Open admin panel window
+            //   Scanner panel = new Scanner(firstName, lastName);
+            //   panel.Show();
+            //   this.Close();
+            //}
+         }
+      }
 
-            return isValid;
-        }
+      private bool ValidateInputs()
+      {
+         bool isValid = true;
 
-        private void SetInvalidInputStyle(TextBox textBox)
-        {
+         if (string.IsNullOrWhiteSpace(FirstNameTextBox.Text) || FirstNameTextBox.Text == "Max")
+         {
+            SetInvalidInputStyle(FirstNameTextBox);
+            isValid = false;
+         }
+         else
+         {
+            SetValidInputStyle(FirstNameTextBox);
+         }
+
+         if (string.IsNullOrWhiteSpace(LastNameTextBox.Text) || LastNameTextBox.Text == "Mustermann")
+         {
+            SetInvalidInputStyle(LastNameTextBox);
+            isValid = false;
+         }
+         else
+         {
+            SetValidInputStyle(LastNameTextBox);
+         }
+
+         return isValid;
+      }
+
+      private void SetInvalidInputStyle(TextBox textBox)
+      {
          textBox.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#C1121C"));
          textBox.Foreground = new SolidColorBrush(Colors.White);
       }
 
-        private void SetValidInputStyle(TextBox textBox)
-        {
+      private void SetValidInputStyle(TextBox textBox)
+      {
          textBox.Background = new SolidColorBrush(Colors.White);
          textBox.Foreground = new SolidColorBrush(Colors.Blue);
       }
 
-        private void FirstNameTextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (textBox.Text == "Max")
-            {
-                textBox.Text = "";
-                textBox.Foreground = new SolidColorBrush(Colors.Blue);
-            }
-            SetValidInputStyle(textBox);
-        }
+      private void FirstNameTextBox_GotFocus(object sender, RoutedEventArgs e)
+      {
+         TextBox textBox = (TextBox)sender;
+         if (textBox.Text == "Max")
+         {
+            textBox.Text = "";
+            textBox.Foreground = new SolidColorBrush(Colors.Blue);
+         }
+         SetValidInputStyle(textBox);
+      }
 
-        private void FirstNameTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (string.IsNullOrWhiteSpace(textBox.Text))
-            {
-                textBox.Text = "Max";
-                textBox.Foreground = new SolidColorBrush(Colors.Gray);
-            }
-            ValidateInputs();
-        }
+      private void FirstNameTextBox_LostFocus(object sender, RoutedEventArgs e)
+      {
+         TextBox textBox = (TextBox)sender;
+         if (string.IsNullOrWhiteSpace(textBox.Text))
+         {
+            textBox.Text = "Max";
+            textBox.Foreground = new SolidColorBrush(Colors.Gray);
+         }
+         ValidateInputs();
+      }
 
-        private void LastNameTextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (textBox.Text == "Mustermann")
-            {
-                textBox.Text = "";
-                textBox.Foreground = new SolidColorBrush(Colors.Blue);
-            }
-            SetValidInputStyle(textBox);
-        }
+      private void LastNameTextBox_GotFocus(object sender, RoutedEventArgs e)
+      {
+         TextBox textBox = (TextBox)sender;
+         if (textBox.Text == "Mustermann")
+         {
+            textBox.Text = "";
+            textBox.Foreground = new SolidColorBrush(Colors.Blue);
+         }
+         SetValidInputStyle(textBox);
+      }
 
-        private void LastNameTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            if (string.IsNullOrWhiteSpace(textBox.Text))
-            {
-                textBox.Text = "Mustermann";
-                textBox.Foreground = new SolidColorBrush(Colors.Gray);
-            }
-            ValidateInputs();
-        }
+      private void LastNameTextBox_LostFocus(object sender, RoutedEventArgs e)
+      {
+         TextBox textBox = (TextBox)sender;
+         if (string.IsNullOrWhiteSpace(textBox.Text))
+         {
+            textBox.Text = "Mustermann";
+            textBox.Foreground = new SolidColorBrush(Colors.Gray);
+         }
+         ValidateInputs();
+      }
 
       private void TextBox_KeyDown(object sender, KeyEventArgs e)
       {
