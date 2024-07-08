f74be8f0d2ab09e88f310ef4105bf4646a78ff99
Sascha Dierl <dierlsasch05@gmail.com>
2024-07-08 09:55:13 +0200

    Dateiimport fertig

--- View/Dateiverwaltung.xaml.cs
+++ View/Dateiverwaltung.xaml.cs
@@ -6,111 +6,112 @@ using System.Windows.Input;
 
 namespace Klimalauf
 {
-   public partial class Dateiverwaltung : Window
-   {
-      //public ObservableCollection<FileItem> files { get; set; }
+    public partial class Dateiverwaltung : Window
+    {
+        //public ObservableCollection<FileItem> files { get; set; }
 
-      private MainViewModel mvmodel;
+        private MainViewModel mvmodel;
 
-      private String firstName;
-      private String lastName;
+        private String firstName;
+        private String lastName;
 
-      public Dateiverwaltung(string firstName, string lastName)
-      {
-         InitializeComponent();
+        public Dateiverwaltung(string firstName, string lastName)
+        {
+            InitializeComponent();
 
-         // Set the ScannerName label with the passed names
-         ScannerName.Content = $"{lastName}, {firstName}";
-         DataContext = this;
-         this.firstName = firstName;
-         this.lastName = lastName;
+            // Set the ScannerName label with the passed names
+            ScannerName.Content = $"{lastName}, {firstName}";
+            this.firstName = firstName;
+            this.lastName = lastName;
 
-         DataContext = new MainViewModel();
-      }
+            this.mvmodel = FindResource("mvmodel") as MainViewModel;
 
-
-      private void LogoutIcon_MouseDown(object sender, MouseButtonEventArgs e)
-      {
-         // Create a new instance of MainWindow
-         MainWindow mainWindow = new MainWindow();
-         mainWindow.Show();
-
-         // Close the current Scanner window
-         this.Close();
-      }
-      private void Window_Loaded(object sender, RoutedEventArgs e)
-      {
-         this.mvmodel = DataContext as MainViewModel;
-
-            mvmodel.LstFiles = new ObservableCollection<FileItem>(FileItem.AlleLesen());
 
         }
 
-      private void UploadFiles_Click(object sender, RoutedEventArgs e)
-      {
-         OpenFileDialog openFileDialog = new OpenFileDialog
-         {
-            Multiselect = true,
-            Filter = "files (*.asv;*.db;*.csv)|*.asv;*.db;*.csv"
-         };
 
-         if (openFileDialog.ShowDialog() == true)
-         {
-            foreach (string fileName in openFileDialog.FileNames)
+        private void LogoutIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Create a new instance of MainWindow
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            // Close the current Scanner window
+            this.Close();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            mvmodel.LstFiles = new ObservableCollection<FileItem>(FileItem.AlleLesen());
+        }
+
+        private void UploadFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
             {
-               string extension = Path.GetExtension(fileName).ToLower();
+                Multiselect = true,
+                Filter = "files (*.asv;*.db;*.csv)|*.asv;*.db;*.csv"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string fileName in openFileDialog.FileNames)
+                {
+                    string extension = Path.GetExtension(fileName).ToLower();
 
                     if (extension == ".asv" || extension == ".db" || extension == ".csv")
                     {
+                        // Datei in die Liste hinzufügen --> wird nicht im UI angezeigt
                         mvmodel.LstFiles.Add(new FileItem
                         {
                             fileName = Path.GetFileName(fileName),
                             uploadDate = DateTime.Now
                         });
+
+                        // Datei in den Dateien-Ordner kopieren
                         Directory.CreateDirectory("Dateien");
                         string destPath = Path.Combine("Dateien", Path.GetFileName(fileName));
                         File.Copy(fileName, destPath, true);
                     }
                     else
                     {
-                  MessageBox.Show($"Die Datei {fileName} hat eine ungültige Erweiterung und kann nicht hochgeladen werden.");
-               }
+                        MessageBox.Show($"Die Datei {fileName} hat eine ungültige Erweiterung und kann nicht hochgeladen werden.");
+                    }
+                }
             }
-         }
-      }
+        }
 
-      private void DeleteSelectedFiles_Click(object sender, RoutedEventArgs e)
-      {
-         var selectedFiles = mvmodel.LstFiles.Where(f => f.IsSelected).ToList();
-         foreach (var file in selectedFiles)
-         {
-            mvmodel.LstFiles.Remove(file);
-         }
-         SelectAllCheckBox.IsChecked = false;
-      }
+        private void DeleteSelectedFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedFiles = mvmodel.LstFiles.Where(f => f.IsSelected).ToList();
+            foreach (var file in selectedFiles)
+            {
+                mvmodel.LstFiles.Remove(file);
+            }
+            SelectAllCheckBox.IsChecked = false;
+        }
 
-      private void SelectAllCheckBox_Click(object sender, RoutedEventArgs e)
-      {
-         bool newValue = (SelectAllCheckBox.IsChecked == true);
-         foreach (var file in mvmodel.LstFiles)
-         {
-            file.IsSelected = newValue;
-         }
+        private void SelectAllCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            bool newValue = (SelectAllCheckBox.IsChecked == true);
+            foreach (var file in mvmodel.LstFiles)
+            {
+                file.IsSelected = newValue;
+            }
 
-         SelectAllTextBlock.Text = SelectAllCheckBox.IsChecked == true ? "Deselect All" : "Select All";
-      }
+            SelectAllTextBlock.Text = SelectAllCheckBox.IsChecked == true ? "Deselect All" : "Select All";
+        }
 
-      private void CloseWindow_Click(object sender, RoutedEventArgs e)
-      {
-         // Open admin panel window
-         AdminScanner adminPanel = new AdminScanner(firstName, lastName);
-         adminPanel.Show();
-         this.Close();
-      }
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            // Open admin panel window
+            AdminScanner adminPanel = new AdminScanner(firstName, lastName);
+            adminPanel.Show();
+            this.Close();
+        }
 
-      private void DownloadFiles_Click(object sender, RoutedEventArgs e)
-      {
+        private void DownloadFiles_Click(object sender, RoutedEventArgs e)
+        {
 
-      }
-   }
+        }
+    }
 }
