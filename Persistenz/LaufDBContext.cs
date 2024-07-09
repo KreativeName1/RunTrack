047a45d12a951df4557aa569198a5dce2fecfc5c
CyberEnte <glisniks@bswiesaude.onmicrosoft.com>
2024-07-09 14:42:21 +0200

    Datenuebersicht verbessert (noch nicht ganz fertig, Klasse und Schule bei Schüler fehlerhaft und Rundengröße fehlt)

--- Persistenz/LaufDBContext.cs
+++ Persistenz/LaufDBContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.IO;
+
 namespace Klimalauf
 {
     public class LaufDBContext : DbContext
     {
         private static string _dbPath = "Dateien/EigeneDatenbank.db";
         public LaufDBContext()
-        : base(GetDbContextOptions())
+            : base(GetDbContextOptions())
         {
             Directory.CreateDirectory("Dateien");
 
@@ -15,10 +18,10 @@ namespace Klimalauf
                 Database.EnsureCreated();
                 SeedTestData();
             }
-
         }
+
         public LaufDBContext(string _path)
-       : base(GetDbContextOptions())
+            : base(GetDbContextOptions())
         {
             _dbPath = _path;
             Database.EnsureCreated();
@@ -32,64 +35,52 @@ namespace Klimalauf
                 benutzer.Vorname = "Sascha";
                 benutzer.Nachname = "Dierl";
 
-                Schule schule1 = new Schule();
-                schule1.Name = "BSZ Wiesau";
+                Schule schule1 = new Schule { Name = "BSZ Wiesau" };
                 db.Schulen.Add(schule1);
                 db.SaveChanges();
 
-                Schule schule2 = new Schule();
-                schule2.Name = "Mittelschule Wiesau";
+                Schule schule2 = new Schule { Name = "Mittelschule Wiesau" };
                 db.Schulen.Add(schule2);
                 db.SaveChanges();
 
-
-
-                RundenArt rundenArt = new RundenArt();
-                rundenArt.Name = "Kurze Runde";
-                rundenArt.LaengeInMeter = 800;
+                RundenArt rundenArt = new RundenArt { Name = "Kurze Runde", LaengeInMeter = 800 };
                 db.RundenArten.Add(rundenArt);
                 db.SaveChanges();
 
-                RundenArt rundenArt2 = new RundenArt();
-                rundenArt2.Name = "Lange Runde";
-                rundenArt2.LaengeInMeter = 1300;
+                RundenArt rundenArt2 = new RundenArt { Name = "Lange Runde", LaengeInMeter = 1300 };
                 db.RundenArten.Add(rundenArt2);
                 db.SaveChanges();
 
-                Klasse klasse1 = new Klasse();
-                klasse1.Name = "BFI10A";
-                klasse1.Schule = schule1;
-                klasse1.RundenArt = rundenArt;
-                klasse1.Jahrgang  = 10;
+                Klasse klasse1 = new Klasse { Name = "BFI10A", Schule = schule1, RundenArt = rundenArt, Jahrgang = 10 };
                 db.Klassen.Add(klasse1);
                 db.SaveChanges();
 
-                Klasse klasse2 = new Klasse();
-                klasse2.Name = "BFI11A";
-                klasse2.Schule = schule1;
-                klasse2.Jahrgang = 11;
-                klasse2.RundenArt = rundenArt2;
+                Klasse klasse2 = new Klasse { Name = "BFI11A", Schule = schule1, RundenArt = rundenArt2, Jahrgang = 11 };
                 db.Klassen.Add(klasse2);
                 db.SaveChanges();
 
                 for (int i = 0; i < 20; i++)
                 {
-                    Schueler schueler = new Schueler();
-                    schueler.Vorname = randomVorname();
-                    schueler.Nachname =  randomNachname();
-                    schueler.Klasse = db.Klassen.First();
-                    schueler.Geschlecht = Geschlecht.Maennlich;
+                    Schueler schueler = new Schueler
+                    {
+                        Vorname = randomVorname(),
+                        Nachname = randomNachname(),
+                        Klasse = klasse1,
+                        Geschlecht = Geschlecht.Maennlich
+                    };
                     db.Schueler.Add(schueler);
                     db.SaveChanges();
                 }
 
                 for (int i = 0; i < 20; i++)
                 {
-                    Schueler schueler = new Schueler();
-                    schueler.Klasse = klasse2;
-                    schueler.Vorname = randomVorname();
-                    schueler.Nachname = randomNachname();
-                    schueler.Geschlecht = Geschlecht.Maennlich;
+                    Schueler schueler = new Schueler
+                    {
+                        Vorname = randomVorname(),
+                        Nachname = randomNachname(),
+                        Klasse = klasse2,
+                        Geschlecht = Geschlecht.Maennlich
+                    };
                     db.Schueler.Add(schueler);
                     db.SaveChanges();
                 }
@@ -101,15 +92,15 @@ namespace Klimalauf
                     DateTime lastTimestamp = DateTime.Now;
                     for (int i = 0; i < rounds; i++)
                     {
-                        Runde runde = new Runde();
-                        runde.Schueler = schueler;
-                        runde.BenutzerName = benutzer.Vorname + " " + benutzer.Nachname;
-                        lastTimestamp = lastTimestamp.AddSeconds(random.Next(60, 1000));
-                        runde.Zeitstempel = lastTimestamp;
+                        Runde runde = new Runde
+                        {
+                            Schueler = schueler,
+                            BenutzerName = benutzer.Vorname + " " + benutzer.Nachname,
+                            Zeitstempel = lastTimestamp.AddSeconds(random.Next(60, 1000))
+                        };
                         db.Runden.Add(runde);
                         db.SaveChanges();
                     }
-
                 }
             }
         }
@@ -118,21 +109,15 @@ namespace Klimalauf
         {
             string[] vorname = { "Max", "Moritz", "Hans", "Peter", "Paul", "Klaus", "Karl", "Kurt", "Kai" };
             Random rnd = new Random();
-            int index = rnd.Next(vorname.Length);
-            return vorname[index];
-
+            return vorname[rnd.Next(vorname.Length)];
         }
 
         private string randomNachname()
         {
             string[] nachname = { "Müller", "Schmidt", "Schneider", "Fischer", "Weber", "Meyer", "Wagner", "Becker", "Schulz" };
             Random rnd = new Random();
-            int index = rnd.Next(nachname.Length);
-            return nachname[index];
+            return nachname[rnd.Next(nachname.Length)];
         }
-
-
-
 
         private static DbContextOptions GetDbContextOptions()
         {
@@ -145,21 +130,23 @@ namespace Klimalauf
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Schule>()
-         .HasMany(s => s.Klassen)
-         .WithOne(k => k.Schule)
-         .HasForeignKey(k => k.SchuleId);
+                .HasMany(s => s.Klassen)
+                .WithOne(k => k.Schule)
+                .HasForeignKey(k => k.SchuleId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Klasse>()
                 .HasMany(k => k.Schueler)
                 .WithOne(s => s.Klasse)
-                .HasForeignKey(s => s.KlasseId);
+                .HasForeignKey(s => s.KlasseId)
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<Schueler>()
                 .HasMany(s => s.Runden)
                 .WithOne(r => r.Schueler)
-                .HasForeignKey(r => r.SchuelerId);
+                .HasForeignKey(r => r.SchuelerId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
-
 
         public DbSet<Klasse> Klassen { get; set; }
         public DbSet<Schule> Schulen { get; set; }
@@ -167,7 +154,5 @@ namespace Klimalauf
         public DbSet<Runde> Runden { get; set; }
         public DbSet<RundenArt> RundenArten { get; set; }
         public DbSet<Benutzer> Benutzer { get; set; }
-
-
     }
 }
--- View/Datenuebersicht.xaml.cs
+++ View/Datenuebersicht.xaml.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
 
 namespace Klimalauf
 {
     /// <summary>
     /// Interaktionslogik für Datenuebersicht.xaml
     /// </summary>
-    public partial class Datenuebersicht : Window 
+    public partial class Datenuebersicht : Window
     {
         DatenuebersichtModel dumodel;
-        private String firstName;
-        private String lastName;
+        private string firstName;
+        private string lastName;
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             this.dumodel = FindResource("dumodel") as DatenuebersichtModel;
+            LoadData();
         }
-
 
         public Datenuebersicht(string firstName, string lastName)
         {
@@ -27,8 +30,8 @@ namespace Klimalauf
             DataContext = this;
             this.firstName = firstName;
             this.lastName = lastName;
-
         }
+
         private void LogoutIcon_MouseDown(object sender, MouseButtonEventArgs e)
         {
             // Create a new instance of MainWindow
@@ -49,209 +52,119 @@ namespace Klimalauf
 
         private void btnBarcodes_Click(object sender, RoutedEventArgs e)
         {
-
+            // Logik für Barcode-Button
         }
 
         private void btnDownload_Click(object sender, RoutedEventArgs e)
         {
-
+            // Logik für Download-Button
         }
 
         private void btnStartseite_Click(object sender, RoutedEventArgs e)
         {
-            btnSchule.Visibility = Visibility.Visible;
-            btnSchuleDisabled.Visibility = Visibility.Collapsed;
-
-            btnKlassen.Visibility = Visibility.Visible;
-            btnKlassenDisabled.Visibility = Visibility.Collapsed;
-
-            btnSchueler.Visibility = Visibility.Visible;
-            btnSchuelerDisabled.Visibility = Visibility.Collapsed;
-
-            btnRunden.Visibility = Visibility.Visible;
-            btnRundenDisabled.Visibility = Visibility.Collapsed;
-
-            btnStartseite.Visibility = Visibility.Collapsed;
-            btnStartseiteDisabled.Visibility = Visibility.Visible;
-
-
-            SchuleGrid.Visibility = Visibility.Collapsed;
-            KlasseGrid.Visibility = Visibility.Collapsed;
-            SchuelerGrid.Visibility = Visibility.Collapsed;
-            RundenGrid.Visibility = Visibility.Collapsed;
-            StartseiteGrid.Visibility = Visibility.Visible;
-
-            btnBarcodes.Visibility = Visibility.Collapsed;
+            SetVisibility(Visibility.Collapsed, Visibility.Collapsed, Visibility.Collapsed, Visibility.Collapsed, Visibility.Visible);
 
             using (var db = new LaufDBContext())
             {
-                // hier zugriff auf die Datenbank
-
-                dumodel.LstSchule = new System.Collections.ObjectModel.ObservableCollection<Schule>(db.Schulen.ToList());
-
-                //// List<Schule> lstSchulen = db.Schulen.ToList();
-                // Schule schule = dumodel.LstSchule.First();
-                // schule.ID;
-
+                dumodel.LstSchule = new ObservableCollection<Schule>(db.Schulen.ToList());
             }
-
         }
 
         private void btnRunden_Click(object sender, RoutedEventArgs e)
         {
-            btnSchule.Visibility = Visibility.Visible;
-            btnSchuleDisabled.Visibility = Visibility.Collapsed;
-
-            btnKlassen.Visibility = Visibility.Visible;
-            btnKlassenDisabled.Visibility = Visibility.Collapsed;
-
-            btnSchueler.Visibility = Visibility.Visible;
-            btnSchuelerDisabled.Visibility = Visibility.Collapsed;
-
-            btnRunden.Visibility = Visibility.Collapsed;
-            btnRundenDisabled.Visibility = Visibility.Visible;
-
-            btnStartseite.Visibility = Visibility.Visible;
-            btnStartseiteDisabled.Visibility = Visibility.Collapsed;
-
-
-            SchuleGrid.Visibility = Visibility.Collapsed;
-            KlasseGrid.Visibility = Visibility.Collapsed;
-            SchuelerGrid.Visibility = Visibility.Collapsed;
-            RundenGrid.Visibility = Visibility.Visible;
-            StartseiteGrid.Visibility = Visibility.Collapsed;
-
-            btnBarcodes.Visibility = Visibility.Collapsed;
+            SetVisibility(Visibility.Collapsed, Visibility.Collapsed, Visibility.Collapsed, Visibility.Visible, Visibility.Collapsed);
 
             using (var db = new LaufDBContext())
             {
-                // hier zugriff auf die Datenbank
-
-                dumodel.LstRunde = new System.Collections.ObjectModel.ObservableCollection<Runde>(db.Runden.ToList());
-
-                //// List<Schule> lstSchulen = db.Schulen.ToList();
-                // Schule schule = dumodel.LstSchule.First();
-                // schule.ID;
-
+                dumodel.LstRunde = new ObservableCollection<Runde>(db.Runden.Include(r => r.Schueler).ThenInclude(s => s.Klasse).ThenInclude(k => k.Schule).ToList());
             }
         }
 
         private void btnSchueler_Click(object sender, RoutedEventArgs e)
         {
-            btnSchule.Visibility = Visibility.Visible;
-            btnSchuleDisabled.Visibility = Visibility.Collapsed;
-
-            btnKlassen.Visibility = Visibility.Visible;
-            btnKlassenDisabled.Visibility = Visibility.Collapsed;
-
-            btnSchueler.Visibility = Visibility.Collapsed;
-            btnSchuelerDisabled.Visibility = Visibility.Visible;
-
-            btnRunden.Visibility = Visibility.Visible;
-            btnRundenDisabled.Visibility = Visibility.Collapsed;
-
-            btnStartseite.Visibility = Visibility.Visible;
-            btnStartseiteDisabled.Visibility = Visibility.Collapsed;
-
-
-            SchuleGrid.Visibility = Visibility.Collapsed;
-            KlasseGrid.Visibility = Visibility.Collapsed;
-            SchuelerGrid.Visibility = Visibility.Visible;
-            RundenGrid.Visibility = Visibility.Collapsed;
-            StartseiteGrid.Visibility = Visibility.Collapsed;
-
-            btnBarcodes.Visibility = Visibility.Visible;
+            SetVisibility(Visibility.Collapsed, Visibility.Collapsed, Visibility.Visible, Visibility.Collapsed, Visibility.Collapsed);
 
             using (var db = new LaufDBContext())
             {
-                // hier zugriff auf die Datenbank
-
-                dumodel.LstSchueler = new System.Collections.ObjectModel.ObservableCollection<Schueler>(db.Schueler.ToList());
-
-                //// List<Schule> lstSchulen = db.Schulen.ToList();
-                // Schule schule = dumodel.LstSchule.First();
-                // schule.ID;
-
+                dumodel.LstSchueler = new ObservableCollection<Schueler>(db.Schueler
+                    .Include(s => s.Klasse)
+                    .ThenInclude(k => k.Schule)
+                    .Include(s => s.Runden)
+                    .ToList());
             }
         }
 
         private void btnKlassen_Click(object sender, RoutedEventArgs e)
         {
-            btnSchule.Visibility = Visibility.Visible;
-            btnSchuleDisabled.Visibility = Visibility.Collapsed;
-
-            btnKlassen.Visibility = Visibility.Collapsed;
-            btnKlassenDisabled.Visibility = Visibility.Visible;
-
-            btnSchueler.Visibility = Visibility.Visible;
-            btnSchuelerDisabled.Visibility = Visibility.Collapsed;
-
-            btnRunden.Visibility = Visibility.Visible;
-            btnRundenDisabled.Visibility = Visibility.Collapsed;
-
-            btnStartseite.Visibility = Visibility.Visible;
-            btnStartseiteDisabled.Visibility = Visibility.Collapsed;
-
-
-            SchuleGrid.Visibility = Visibility.Collapsed;
-            KlasseGrid.Visibility = Visibility.Visible;
-            SchuelerGrid.Visibility = Visibility.Collapsed;
-            RundenGrid.Visibility = Visibility.Collapsed;
-            StartseiteGrid.Visibility = Visibility.Collapsed;
-
-            btnBarcodes.Visibility = Visibility.Collapsed;
+            SetVisibility(Visibility.Collapsed, Visibility.Visible, Visibility.Collapsed, Visibility.Collapsed, Visibility.Collapsed);
 
             using (var db = new LaufDBContext())
             {
-                // hier zugriff auf die Datenbank
+                var klassenMitDetails = db.Klassen
+                                          .Include(k => k.Schule)
+                                          .Include(k => k.Schueler)
+                                          .ThenInclude(s => s.Runden)
+                                          .ToList();
 
-                dumodel.LstKlasse = new System.Collections.ObjectModel.ObservableCollection<Klasse>(db.Klassen.ToList());
+                dumodel.LstKlasse = new ObservableCollection<Klasse>(klassenMitDetails);
 
-                //// List<Schule> lstSchulen = db.Schulen.ToList();
-                // Schule schule = dumodel.LstSchule.First();
-                // schule.ID;
-
+                if (dumodel.LstKlasse.Any())
+                {
+                    var ersteSchule = dumodel.LstKlasse.First().Schule;
+                    if (ersteSchule != null)
+                    {
+                        var schuleId = ersteSchule.Id;
+                        Console.WriteLine($"Die ID der ersten Schule ist: {schuleId}");
+                    }
+                }
             }
         }
 
         private void btnSchule_Click(object sender, RoutedEventArgs e)
         {
-            btnSchule.Visibility = Visibility.Collapsed;
-            btnSchuleDisabled.Visibility = Visibility.Visible;
-
-            btnKlassen.Visibility = Visibility.Visible;
-            btnKlassenDisabled.Visibility = Visibility.Collapsed;
-
-            btnSchueler.Visibility = Visibility.Visible;
-            btnSchuelerDisabled.Visibility = Visibility.Collapsed;
-
-            btnRunden.Visibility = Visibility.Visible;
-            btnRundenDisabled.Visibility = Visibility.Collapsed;
-
-            btnStartseite.Visibility = Visibility.Visible;
-            btnStartseiteDisabled.Visibility = Visibility.Collapsed;
-
-
-            SchuleGrid.Visibility = Visibility.Visible;
-            KlasseGrid.Visibility = Visibility.Collapsed;
-            SchuelerGrid.Visibility = Visibility.Collapsed;
-            RundenGrid.Visibility = Visibility.Collapsed;
-            StartseiteGrid.Visibility = Visibility.Collapsed;
-
-            btnBarcodes.Visibility = Visibility.Collapsed;
+            SetVisibility(Visibility.Visible, Visibility.Collapsed, Visibility.Collapsed, Visibility.Collapsed, Visibility.Collapsed);
 
             using (var db = new LaufDBContext())
             {
-                // hier zugriff auf die Datenbank
-
-                dumodel.LstSchule = new System.Collections.ObjectModel.ObservableCollection<Schule>(db.Schulen.ToList());
-
-               //// List<Schule> lstSchulen = db.Schulen.ToList();
-               // Schule schule = dumodel.LstSchule.First();
-               // schule.ID;
-
+                dumodel.LstSchule = new ObservableCollection<Schule>(db.Schulen.ToList());
             }
         }
-        
+
+        private void SetVisibility(Visibility schuleGrid, Visibility klasseGrid, Visibility schuelerGrid, Visibility rundenGrid, Visibility startseiteGrid)
+        {
+            SchuleGrid.Visibility = schuleGrid;
+            KlasseGrid.Visibility = klasseGrid;
+            SchuelerGrid.Visibility = schuelerGrid;
+            RundenGrid.Visibility = rundenGrid;
+            StartseiteGrid.Visibility = startseiteGrid;
+
+            btnSchule.Visibility = schuleGrid == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            btnSchuleDisabled.Visibility = schuleGrid == Visibility.Visible ? Visibility.Visible : Visibility.Collapsed;
+
+            btnKlassen.Visibility = klasseGrid == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            btnKlassenDisabled.Visibility = klasseGrid == Visibility.Visible ? Visibility.Visible : Visibility.Collapsed;
+
+            btnSchueler.Visibility = schuelerGrid == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            btnSchuelerDisabled.Visibility = schuelerGrid == Visibility.Visible ? Visibility.Visible : Visibility.Collapsed;
+
+            btnRunden.Visibility = rundenGrid == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            btnRundenDisabled.Visibility = rundenGrid == Visibility.Visible ? Visibility.Visible : Visibility.Collapsed;
+
+            btnStartseite.Visibility = startseiteGrid == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            btnStartseiteDisabled.Visibility = startseiteGrid == Visibility.Visible ? Visibility.Visible : Visibility.Collapsed;
+
+            btnBarcodes.Visibility = schuelerGrid == Visibility.Visible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void LoadData()
+        {
+            using (var db = new LaufDBContext())
+            {
+                dumodel.LstSchule = new ObservableCollection<Schule>(db.Schulen.ToList());
+                dumodel.LstKlasse = new ObservableCollection<Klasse>(db.Klassen.Include(k => k.Schule).Include(k => k.Schueler).ThenInclude(s => s.Runden).ToList());
+                dumodel.LstSchueler = new ObservableCollection<Schueler>(db.Schueler.Include(s => s.Klasse).ThenInclude(k => k.Schule).Include(s => s.Runden).ToList());
+                dumodel.LstRunde = new ObservableCollection<Runde>(db.Runden.Include(r => r.Schueler).ThenInclude(s => s.Klasse).ThenInclude(k => k.Schule).ToList());
+            }
+        }
     }
 }
