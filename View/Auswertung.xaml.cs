d2b66b76cd0a66a6c6ace970090e4a722a4f56df
Sascha Dierl <dierlsasch05@gmail.com>
2024-07-12 13:09:34 +0200

    auswertung weitergemacht

--- View/Auswertung.xaml.cs
+++ View/Auswertung.xaml.cs
@@ -1,17 +1,8 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Klimalauf
 {
@@ -20,41 +11,207 @@ namespace Klimalauf
     /// </summary>
     public partial class Auswertung : Window
     {
-        private string[] pfade;
+        private string[] _pfade;
+        private List<RadioButton> _rundenArten = new List<RadioButton>();
+        private AuswertungModel _amodel;
+        private MainViewModel _mvmodel;
         public Auswertung()
         {
-            InitializeComponent();
-            pfade = System.IO.Directory.GetFiles("Dateien", "*.db");
-            LoadData();
+            _pfade = System.IO.Directory.GetFiles("Dateien", "*.db");
+            _amodel = FindResource("amodel") as AuswertungModel;
+            _mvmodel = FindResource("mvmodel") as MainViewModel;
 
-            
+            InitializeComponent();
+
+
+            _amodel.Liste = new ObservableCollection<object>();
+            LoadData();
+            init();
+
+
+        }
+        public void init()
+        {
+            btnImport.Click += (s, e) =>
+            {
+                Dateiverwaltung dateiverwaltung = new Dateiverwaltung();
+                dateiverwaltung.Show();
+
+            };
+            btnExport.Click += (s, e) =>
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "files (*.db)|*.db",
+                    FileName = "Auswertung.db"
+                };
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    System.IO.File.Copy("internal.db", saveFileDialog.FileName);
+                }
+            };
+            btnSchliessen.Click += (s, e) =>
+            {
+                Scanner scanner = new Scanner();
+                scanner.Show();
+                this.Close();
+            };
+            btnDiagramm.Click += (s, e) =>
+            {
+
+            };
+            btnWertung.Click += (s, e) =>
+            {
+
+            };
+            btnSchuelerWertung.Click += (s, e) =>
+            {
+
+            };
         }
 
         public void LoadData()
         {
-            using (var db = new MergedDBContext(pfade))
+            using (var db = new MergedDBContext(_pfade))
             {
                 bool first = true;
-                if (db.RundenArten.Count() == 0) RundenGroesse.Children.Add(new Label {Content = "Keine Rundenarten vorhanden" });
+                if (db.RundenArten.Count() == 0) RundenGroesse.Children.Add(new Label { Content = "Keine Rundenarten vorhanden" });
                 else foreach (RundenArt rundenArt in db.RundenArten)
-                {
-                    RadioButton rb = new RadioButton
                     {
-                        Content = rundenArt.Name,
-                        Name = rundenArt.Name.Replace(" ", "_"),
-                        IsChecked = first
-                    };
-                    RundenGroesse.Children.Add(rb);
-                    first = false;
-                }
+                        RadioButton rb = new RadioButton
+                        {
+                            Content = rundenArt.Name,
+                            Name = rundenArt.Name.Replace(" ", "_"),
+                            IsChecked = first
+                        };
+                        _rundenArten.Add(rb);
+                        RundenGroesse.Children.Add(rb);
+                        first = false;
+                    }
 
-                cbSchule.ItemsSource = db.Schulen.ToList();
-                cbSchule.SelectedIndex = 0;
-                cbKlasse.ItemsSource = db.Klassen.ToList();
-                cbKlasse.SelectedIndex = 0;
-                iudJahrgang.Value = 0;
+                _amodel.Schulen = new ObservableCollection<Schule>(db.Schulen.ToList());
+                _amodel.Klassen = new ObservableCollection<Klasse>(db.Klassen.ToList());
+                _amodel.SelectedSchule = _amodel.Schulen.FirstOrDefault();
+                _amodel.SelectedKlasse = _amodel.Klassen.FirstOrDefault();
+
 
             }
+        }
+
+        private void change(object sender, RoutedEventArgs e)
+        {
+            using (var db = new MergedDBContext(_pfade))
+            {
+
+                if (_amodel.IsInsgesamt)
+                {
+                    _amodel.newList();
+                    // get schueler with runden, klasse, klasse->rundenart with intersect/include
+                    foreach (Schueler schueler in db.Schueler.Include(s => s.Runden).Include(s => s.Klasse).Where(s => s.Runden.Count() > 1))
+                    {
+                        string bewertung = GetBewertung(schueler);
+                        _amodel.Liste.Add(new { Name = schueler.Vorname + " " + schueler.Nachname, Schule = schueler.Klasse.Schule.Name, Klasse = schueler.Klasse.Name, Bewertung = bewertung });
+                    }
+                }
+                else if (_amodel.IsSchule)
+                {
+                    _amodel.newList();
+                    foreach (Schueler schueler in db.Schueler.Include(s => s.Runden).Include(s => s.Klasse).Where(s => s.Klasse.Schule == _amodel.SelectedSchule && s.Runden.Count() > 1))
+                    {
+                        string bewertung = GetBewertung(schueler);
+                        _amodel.Liste.Add(new { Name = schueler.Vorname + " " + schueler.Nachname, Klasse = schueler.Klasse.Name, Bewertung = bewertung });
+                    }
+                }
+                else if (_amodel.IsKlasse)
+                {
+                    _amodel.newList();
+                    foreach (Schueler schueler in db.Schueler.Include(s => s.Runden).Include(s => s.Klasse).Where(s => s.Klasse == _amodel.SelectedKlasse && s.Runden.Count() > 1))
+                    {
+                        string bewertung = GetBewertung(schueler);
+                        _amodel.Liste.Add(new { Name = schueler.Vorname + " " + schueler.Nachname, Bewertung = bewertung });
+                    }
+                }
+                else if (_amodel.IsJahrgang)
+                {
+                    _amodel.newList();
+                    foreach (Schueler schueler in db.Schueler.Include(s => s.Runden).Include(s => s.Klasse).Where(s => s.Klasse.Jahrgang == _amodel.Jahrgang && s.Runden.Count > 1))
+                    {
+                        string bewertung = GetBewertung(schueler);
+                        _amodel.Liste.Add(new { Name = schueler.Vorname + " " + schueler.Nachname, Klasse = schueler.Klasse.Name, Schule = schueler.Klasse.Schule.Name, Bewertung = bewertung });
+                    }
+                }
+            }
+        }
+
+        private void iudJahrgang_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            change(sender, e);
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            change(sender, e);
+        }
+
+        private string GetBewertung(Schueler schueler)
+        {
+            string Bewertung = "";
+            if (_amodel.IsAnzahl)
+            {
+                Bewertung = Convert.ToString(schueler.Runden.Where(r => r.Schueler == schueler).Count()-1);
+            }
+            else if (_amodel.IsZeit)
+            {
+                List<TimeSpan> rundenZeiten = new List<TimeSpan>();
+
+                for (int i = 1; i < schueler.Runden.Count; i++)
+                {
+                    TimeSpan rundenzeit = schueler.Runden[i].Zeitstempel - schueler.Runden[i - 1].Zeitstempel;
+                    rundenZeiten.Add(rundenzeit);
+                }
+
+                // get best time and worst time
+                if (rundenZeiten.Count > 0)
+                {
+                    TimeSpan schnellsteRunde = rundenZeiten[0];
+                    TimeSpan langsamsteRunde = rundenZeiten[0];
+                    int indexSchnellsteRunde = 0;
+                    int indexLangsamsteRunde = 0;
+                    TimeSpan gesamtZeit = new TimeSpan(0, 0, 0);
+
+                    for (int i = 1; i < rundenZeiten.Count; i++)
+                    {
+                        gesamtZeit += rundenZeiten[i];
+
+                        if (rundenZeiten[i] < schnellsteRunde)
+                        {
+                            schnellsteRunde = rundenZeiten[i];
+                            indexSchnellsteRunde = i;
+                        }
+
+                        if (rundenZeiten[i] > langsamsteRunde)
+                        {
+                            langsamsteRunde = rundenZeiten[i];
+                            indexLangsamsteRunde = i;
+                        }
+                    }
+
+                    TimeSpan durchschnittsZeit = new TimeSpan(gesamtZeit.Ticks / rundenZeiten.Count);
+                    if (_amodel.IsZeitSchnellste) Bewertung = schnellsteRunde.ToString(@"mm\:ss");
+                    else if (_amodel.IsZeitLangsamste) Bewertung = langsamsteRunde.ToString(@"mm\:ss");
+                    else if (_amodel.IsZeitDurchschnitt && durchschnittsZeit.Ticks > 0) Bewertung = durchschnittsZeit.ToString(@"mm\:ss");
+                    else Bewertung = "--:--";
+                }
+                else
+                {
+                    Bewertung = "--:--";
+                }
+            }
+            else if (_amodel.IsDistanz)
+            {
+                Bewertung = ((schueler.Runden.Count()-1) * schueler.Klasse.RundenArt.LaengeInMeter).ToString("#,##0") + " m";
+            }
+            return Bewertung;
         }
     }
 }
---
+++ ViewModel/AuswertungModel.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klimalauf
+{
+    public class AuswertungModel : BaseModel
+    {
+        private bool _isMaennlich { get; set; } = false;
+        private bool _isWeiblich { get; set; } = false;
+        private bool _isGesamt { get; set; } = true;
+
+        private bool _isAnzahl { get; set; } = true;
+        private bool _isZeit { get; set; } = false;
+        private bool _isDistanz { get; set; } = false;
+
+        private bool _isInsgesamt { get; set; } = true;
+        private bool _isSchule { get; set; } = false;
+        private bool _isKlasse { get; set; } = false;
+        private bool _isJahrgang { get; set; } = false;
+
+        private bool _isZeitSchnellste { get; set; } = true;
+        private bool _isZeitLangsamste { get; set; } = false;
+        private bool _isZeitDurchschnitt { get; set; } = false;
+
+        private ObservableCollection<object> _liste { get; set; } = new ObservableCollection<object>();
+        private object _selectedItem { get; set; } = null;
+
+        private int _jahrgang { get; set; } = 0;
+        private ObservableCollection<Schule> _schulen { get; set; } = new ObservableCollection<Schule>();
+        private Schule? _selectedSchule { get; set; } = null;
+        private ObservableCollection<Klasse> _klassen { get; set; } = new ObservableCollection<Klasse>();
+        private Klasse? _selectedKlasse { get; set; } = null;
+
+        public bool IsMaennlich
+        {
+            get { return _isMaennlich; }
+            set { _isMaennlich = value; OnPropertyChanged("IsMaennlich"); }
+        }
+        public bool IsWeiblich
+        {
+            get { return _isWeiblich; }
+            set { _isWeiblich = value; OnPropertyChanged("IsWeiblich"); }
+        }
+        public bool IsGesamt
+        {
+            get { return _isGesamt; }
+            set { _isGesamt = value; OnPropertyChanged("IsGesamt"); }
+        }
+
+        public bool IsAnzahl
+        {
+            get { return _isAnzahl; }
+            set { _isAnzahl = value; OnPropertyChanged("IsAnzahl"); }
+        }
+        public bool IsZeit
+        {
+            get { return _isZeit; }
+            set { _isZeit = value; OnPropertyChanged("IsZeit"); }
+        }
+        public bool IsDistanz
+        {
+            get { return _isDistanz; }
+            set { _isDistanz = value; OnPropertyChanged("IsDistanz"); }
+        }
+
+
+        public bool IsInsgesamt
+        {
+            get { return _isInsgesamt; }
+            set { _isInsgesamt = value; OnPropertyChanged("IsInsgesamt"); }
+        }
+        public bool IsSchule
+        {
+            get { return _isSchule; }
+            set { _isSchule = value; OnPropertyChanged("IsSchule"); }
+        }
+        public bool IsKlasse
+        {
+            get { return _isKlasse; }
+            set { _isKlasse = value; OnPropertyChanged("IsKlasse"); }
+        }
+        public bool IsJahrgang
+        {
+            get { return _isJahrgang; }
+            set { _isJahrgang = value; OnPropertyChanged("IsJahrgang"); }
+        }
+
+        public bool IsZeitSchnellste
+        {
+            get { return _isZeitSchnellste; }
+            set { _isZeitSchnellste = value; OnPropertyChanged("IsZeitSchnellste"); }
+        }
+        public bool IsZeitLangsamste
+        {
+            get { return _isZeitLangsamste; }
+            set { _isZeitLangsamste = value; OnPropertyChanged("IsZeitLangsamste"); }
+        }
+        public bool IsZeitDurchschnitt
+        {
+            get { return _isZeitDurchschnitt; }
+            set { _isZeitDurchschnitt = value; OnPropertyChanged("IsZeitDurchschnitt"); }
+        }
+
+
+
+        public int Jahrgang
+        {
+            get { return _jahrgang; }
+            set { _jahrgang = value; OnPropertyChanged("Jahrgang"); }
+        }
+
+        public ObservableCollection<Schule> Schulen
+        {
+            get { return _schulen; }
+            set { _schulen = value; OnPropertyChanged("Schulen"); }
+        }
+
+        public Schule? SelectedSchule
+        {
+            get { return _selectedSchule; }
+            set { _selectedSchule = value; OnPropertyChanged("SelectedSchule"); }
+        }
+
+        public ObservableCollection<Klasse> Klassen
+        {
+            get { return _klassen; }
+            set { _klassen = value; OnPropertyChanged("Klassen"); }
+        }
+
+        public Klasse? SelectedKlasse
+        {
+            get { return _selectedKlasse; }
+            set { _selectedKlasse = value; OnPropertyChanged("SelectedKlasse"); }
+        }
+
+        public ObservableCollection<object> Liste
+        {
+            get { return _liste; }
+            set { _liste = value; OnPropertyChanged("Liste"); }
+        }
+
+        public object SelectedItem
+        {
+            get { return _selectedItem; }
+            set { _selectedItem = value; OnPropertyChanged("SelectedItem"); }
+        }
+
+        public void newList()
+        {
+            Liste = new ObservableCollection<object>();
+        }
+    }
+
+}
