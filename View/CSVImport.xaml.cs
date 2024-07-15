c984ec3bb5120c8d1832a826c30d10fc3c292d4e
Sascha Dierl <dierlsasch05@gmail.com>
2024-07-15 14:03:49 +0200

    CSVImport Seite erstellt, Obere Leiste in ein UserControl verschoben (muss in allen Seiten noch implementiert werden)

---
+++ View/CSVImport.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Klimalauf
+{
+    /// <summary>
+    /// Interaktionslogik für CSVImport.xaml
+    /// </summary>
+    public partial class CSVImport : Window
+    {
+        private MainViewModel _mvmodel;
+        public CSVImport()
+        {
+            _mvmodel = FindResource("mvmodel") as MainViewModel;
+            InitializeComponent();
+            Leiste.Benutzername = _mvmodel.Benutzer.Vorname + ", " + _mvmodel.Benutzer.Nachname;
+        }
+    }
+}
---
+++ View/TopBar.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Klimalauf
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class TopBar : UserControl
+    {
+        public string Benutzername { get; set; } = "Vorname, Nachname";
+
+        public TopBar()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+        }
+    }
+}
