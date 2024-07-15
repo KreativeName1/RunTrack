f9fdeb2dbba30d9e2b0ea964aff0b41239dc2839
Paul Fischer <fischerp@bswiesaude.onmicrosoft.com>
2024-07-15 15:14:10 +0200

    Einstellungsfenster Verwaltungsfenster hinzugefügt, aber noch nicht fertig vom Ausshene und der Logik

---
+++ View/VerwaltungRunden.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Klimalauf.View
+{
+    /// <summary>
+    /// Interaktionslogik für VerwaltungRunden.xaml
+    /// </summary>
+    public partial class VerwaltungRunden : Window
+    {
+        public VerwaltungRunden()
+        {
+            InitializeComponent();
+        }
+    }
+}
