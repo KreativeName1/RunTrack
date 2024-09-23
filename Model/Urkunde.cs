11ace5e24f416c5765d59493c3895ebdfc3a0607
= <=>
2024-09-23 14:40:52 +0200

    Urkunde fast fertig (Stürzt ab wenn Seite zu klein)

--- Model/Urkunde.cs
+++ Model/Urkunde.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RunTrack
+﻿namespace RunTrack
 {
-    public class Urkunde
-    {
-        public string LaufName { get; set; } = string.Empty;
-        public string Kategorie { get; set; } = string.Empty;
-        public string Auswertungsart { get; set; } = string.Empty;
-        public string Wert { get; set; } = string.Empty;
-        public string Platzierung { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
+	public class Urkunde
+	{
+		public string LaufName { get; set; } = string.Empty;
+		public string Kategorie { get; set; } = string.Empty;
+		public string Auswertungsart { get; set; } = string.Empty;
+		public string Wert { get; set; } = string.Empty;
+		public string Platzierung { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+		public string Geschlecht { get; set; } = "Insgesamt";
 
 
-        public Urkunde() { }
+		public Urkunde() { }
 
-        public Urkunde(string laufName, string auswertungsart, string kategorie, string wert, string platzierung, string name )
-        {
-            LaufName = laufName;
-            Kategorie = kategorie;
-            Auswertungsart = auswertungsart;
-            Wert = wert;
-            Platzierung = platzierung;
-            Name = name;
-        }
-    }
+		public Urkunde(string laufName, string auswertungsart, string kategorie, string wert, string platzierung, string name, string geschlecht)
+		{
+			LaufName = laufName;
+			Kategorie = kategorie;
+			Auswertungsart = auswertungsart;
+			Wert = wert;
+			Platzierung = platzierung;
+			Name = name;
+			Geschlecht = geschlecht;
+		}
+	}
 }
