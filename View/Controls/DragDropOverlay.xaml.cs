5b04a8b322b56942eb45d9e897f8f930c194d687
Sascha Dierl <dierlsasch05@gmail.com>
2024-10-14 10:43:41 +0200

    Import von Dateien mit Drag&Drop

---
+++ View/Controls/DragDropOverlay.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RunTrack
+{
+    /// <summary>
+    /// Interaktionslogik für DragDropOverlay.xaml
+    /// </summary>
+    public partial class DragDropOverlay : UserControl
+    {
+        public DragDropOverlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
