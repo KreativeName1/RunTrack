d8b3c184c09a0033332121240b3b34d7f1a49ead
fischerp <fischerp@NUCD21021>
2024-10-15 09:55:13 +0200

    zwischenspeichern

--- View/AdminVerwalten.xaml.cs
+++ View/AdminVerwalten.xaml.cs
@@ -1,193 +1,185 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RunTrack.View
 {
-   /// <summary>
-   /// Interaktionslogik für AdminVerwalten.xaml
-   /// </summary>
-   public partial class AdminVerwalten : Page
-   {
-      private MainModel? _mmodel;
+    /// <summary>
+    /// Interaktionslogik für AdminVerwalten.xaml
+    /// </summary>
+    public partial class AdminVerwalten : Page
+    {
+        private MainModel? _mmodel;
         private AdminModel _model;
         private LaufDBContext _db = new();
         public AdminVerwalten()
-      {
-         InitializeComponent();
-         _model = FindResource("admodel") as AdminModel ?? new();
+        {
+            InitializeComponent();
+            _model = FindResource("admodel") as AdminModel ?? new();
 
-         _mmodel = FindResource("pmodel") as MainModel ?? new();
-         this.btnAbbrechen.Click += (sender, e) => _mmodel?.Navigate(_mmodel.History[^1]);
-         LoadContent();
-      }
+            _mmodel = FindResource("pmodel") as MainModel ?? new();
+            this.btnAbbrechen.Click += (sender, e) => _mmodel?.Navigate(_mmodel.History[^1]);
+            LoadContent();
 
-      private void LoadContent()
-      {
-         //using (var db = new LaufDBContext())
-         //{
-         //   var sortedUser = db.Benutzer.OrderBy(r => r.Id).ToList();
-         //   int rowIndex = 0;
+            
+        }
 
-         //   foreach (Benutzer benutzer in sortedUser)
-         //   {
-         //      // Zeilen hinzufügen
-         //      GridSettings.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+        private void LoadContent()
+        {
+            //using (var db = new LaufDBContext())
+            //{
+            //   var sortedUser = db.Benutzer.OrderBy(r => r.Id).ToList();
+            //   int rowIndex = 0;
 
-         //      Label label = new()
-         //      {
-         //         Content = benutzer.Id,
-         //         FontSize = 14,
-         //         MinWidth = 25,
-         //         HorizontalAlignment = HorizontalAlignment.Right,
-         //         VerticalAlignment = VerticalAlignment.Center,
-         //         Margin = new Thickness(0, 2.5, 0, 2.5)
-         //      };
-         //      Grid.SetRow(label, rowIndex);
-         //      Grid.SetColumn(label, 0);
-         //      GridSettings.Children.Add(label);
+            //   foreach (Benutzer benutzer in sortedUser)
+            //   {
+            //      // Zeilen hinzufügen
+            //      GridSettings.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
-         //      Label labelSpacer = new()
-         //      {
-         //         Content = "→",
-         //         FontSize = 14,
-         //         Width = 25,
-         //         HorizontalAlignment = HorizontalAlignment.Right,
-         //         VerticalAlignment = VerticalAlignment.Center,
-         //         Margin = new Thickness(0, 2.5, 8, 2.5)
-         //      };
-         //      Grid.SetRow(labelSpacer, rowIndex);
-         //      Grid.SetColumn(labelSpacer, 1);
-         //      GridSettings.Children.Add(labelSpacer);
+            //      Label label = new()
+            //      {
+            //         Content = benutzer.Id,
+            //         FontSize = 14,
+            //         MinWidth = 25,
+            //         HorizontalAlignment = HorizontalAlignment.Right,
+            //         VerticalAlignment = VerticalAlignment.Center,
+            //         Margin = new Thickness(0, 2.5, 0, 2.5)
+            //      };
+            //      Grid.SetRow(label, rowIndex);
+            //      Grid.SetColumn(label, 0);
+            //      GridSettings.Children.Add(label);
 
-         //      Label label2 = new()
-         //      {
-         //         Content = benutzer.Vorname,
-         //         FontSize = 14,
-         //         MinWidth = 100,
-         //         HorizontalAlignment = HorizontalAlignment.Left,
-         //         VerticalAlignment = VerticalAlignment.Center,
-         //         Margin = new Thickness(0, 2.5, 0, 2.5)
-         //      };
+            //      Label labelSpacer = new()
+            //      {
+            //         Content = "→",
+            //         FontSize = 14,
+            //         Width = 25,
+            //         HorizontalAlignment = HorizontalAlignment.Right,
+            //         VerticalAlignment = VerticalAlignment.Center,
+            //         Margin = new Thickness(0, 2.5, 8, 2.5)
+            //      };
+            //      Grid.SetRow(labelSpacer, rowIndex);
+            //      Grid.SetColumn(labelSpacer, 1);
+            //      GridSettings.Children.Add(labelSpacer);
 
-         //      Grid.SetRow(label2, rowIndex);
-         //      Grid.SetColumn(label2, 2);
-         //      GridSettings.Children.Add(label2);
+            //      Label label2 = new()
+            //      {
+            //         Content = benutzer.Vorname,
+            //         FontSize = 14,
+            //         MinWidth = 100,
+            //         HorizontalAlignment = HorizontalAlignment.Left,
+            //         VerticalAlignment = VerticalAlignment.Center,
+            //         Margin = new Thickness(0, 2.5, 0, 2.5)
+            //      };
 
-         //      Label labelSeconds = new()
-         //      {
-         //         Content = benutzer.Nachname,
-         //         FontSize = 14,
-         //         MinWidth = 100,
-         //         HorizontalAlignment = HorizontalAlignment.Left,
-         //         VerticalAlignment = VerticalAlignment.Center,
-         //         Margin = new Thickness(0, 2.5, 0, 2.5)
-         //      };
-         //      Grid.SetRow(labelSeconds, rowIndex);
-         //      Grid.SetColumn(labelSeconds, 3);
-         //      GridSettings.Children.Add(labelSeconds);
+            //      Grid.SetRow(label2, rowIndex);
+            //      Grid.SetColumn(label2, 2);
+            //      GridSettings.Children.Add(label2);
 
-         //      // Löschen
-         //      Button deleteButton = new()
-         //      {
-         //         Content = "🗑️",
-         //         FontSize = 12,
-         //         Width = 25,
-         //         Height = 25,
-         //         VerticalAlignment = VerticalAlignment.Center,
-         //         HorizontalAlignment = HorizontalAlignment.Left,
-         //         Margin = new Thickness(30, 2.5, 5, 2.5),
-         //         Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F4F8F4")),
-         //         ToolTip = "Löschen",
-         //      };
-         //      Image buttonImageDelete = new()
-         //      {
-         //         Source = new BitmapImage(new Uri("pack://application:,,,/Images/buttonIcons/delete.png")),
-         //         Width = 20,
-         //         Height = 20
-         //      };
-         //      deleteButton.Content = buttonImageDelete;
-         //      deleteButton.Click += DeleteButton_Click;
+            //      Label labelSeconds = new()
+            //      {
+            //         Content = benutzer.Nachname,
+            //         FontSize = 14,
+            //         MinWidth = 100,
+            //         HorizontalAlignment = HorizontalAlignment.Left,
+            //         VerticalAlignment = VerticalAlignment.Center,
+            //         Margin = new Thickness(0, 2.5, 0, 2.5)
+            //      };
+            //      Grid.SetRow(labelSeconds, rowIndex);
+            //      Grid.SetColumn(labelSeconds, 3);
+            //      GridSettings.Children.Add(labelSeconds);
 
-         //      Grid.SetRow(deleteButton, rowIndex);
-         //      Grid.SetColumn(deleteButton, 4);
-         //      GridSettings.Children.Add(deleteButton);
+            //      // Löschen
+            //      Button deleteButton = new()
+            //      {
+            //         Content = "🗑️",
+            //         FontSize = 12,
+            //         Width = 25,
+            //         Height = 25,
+            //         VerticalAlignment = VerticalAlignment.Center,
+            //         HorizontalAlignment = HorizontalAlignment.Left,
+            //         Margin = new Thickness(30, 2.5, 5, 2.5),
+            //         Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F4F8F4")),
+            //         ToolTip = "Löschen",
+            //      };
+            //      Image buttonImageDelete = new()
+            //      {
+            //         Source = new BitmapImage(new Uri("pack://application:,,,/Images/buttonIcons/delete.png")),
+            //         Width = 20,
+            //         Height = 20
+            //      };
+            //      deleteButton.Content = buttonImageDelete;
+            //      deleteButton.Click += DeleteButton_Click;
 
-         //      // Einstellungen
-         //      Button optionsButton = new()
-         //      {
-         //         Width = 25,
-         //         Height = 25,
-         //         VerticalAlignment = VerticalAlignment.Center,
-         //         HorizontalAlignment = HorizontalAlignment.Left,
-         //         Margin = new Thickness(0, 2.5, 10, 2.5),
-         //         Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F4F8F4")),
-         //         ToolTip = "Einstellungen",
-         //      };
-         //      Image buttonImageOptions = new()
-         //      {
-         //         Source = new BitmapImage(new Uri("pack://application:,,,/Images/buttonIcons/settings.png")),
-         //         Width = 20,
-         //         Height = 20
-         //      };
+            //      Grid.SetRow(deleteButton, rowIndex);
+            //      Grid.SetColumn(deleteButton, 4);
+            //      GridSettings.Children.Add(deleteButton);
 
-         //      optionsButton.Content = buttonImageOptions;
+            //      // Einstellungen
+            //      Button optionsButton = new()
+            //      {
+            //         Width = 25,
+            //         Height = 25,
+            //         VerticalAlignment = VerticalAlignment.Center,
+            //         HorizontalAlignment = HorizontalAlignment.Left,
+            //         Margin = new Thickness(0, 2.5, 10, 2.5),
+            //         Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F4F8F4")),
+            //         ToolTip = "Einstellungen",
+            //      };
+            //      Image buttonImageOptions = new()
+            //      {
+            //         Source = new BitmapImage(new Uri("pack://application:,,,/Images/buttonIcons/settings.png")),
+            //         Width = 20,
+            //         Height = 20
+            //      };
 
-         //      optionsButton.Click += OptionsButton_Click;
+            //      optionsButton.Content = buttonImageOptions;
 
-         //      Grid.SetRow(optionsButton, rowIndex);
-         //      Grid.SetColumn(optionsButton, 5);
-         //      GridSettings.Children.Add(optionsButton);
+            //      optionsButton.Click += OptionsButton_Click;
 
-
-         //      rowIndex++;
-
-         //      // Add Rectangle only between rows, except after the last row
-         //      if (rowIndex < sortedUser.Count)
-         //      {
-         //         Rectangle rect = new()
-         //         {
-         //            Height = 1,
-         //            Fill = new SolidColorBrush(Colors.Gray),
-         //            Margin = new Thickness(0, 0, 0, 36.5)
-         //         };
-
-         //         Grid.SetRow(rect, rowIndex);
-         //         Grid.SetColumnSpan(rect, 6);
-         //         GridSettings.Children.Add(rect);
-         //      }
-         //   }
-         //}
-      }
+            //      Grid.SetRow(optionsButton, rowIndex);
+            //      Grid.SetColumn(optionsButton, 5);
+            //      GridSettings.Children.Add(optionsButton);
 
 
-      private void OptionsButton_Click(object sender, RoutedEventArgs e)
-      {
-      }
+            //      rowIndex++;
 
-      private void IntegerUpDown_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-      {
-      }
+            //      // Add Rectangle only between rows, except after the last row
+            //      if (rowIndex < sortedUser.Count)
+            //      {
+            //         Rectangle rect = new()
+            //         {
+            //            Height = 1,
+            //            Fill = new SolidColorBrush(Colors.Gray),
+            //            Margin = new Thickness(0, 0, 0, 36.5)
+            //         };
 
-      //private void AddButton_Click(object sender, RoutedEventArgs e)
-      //{
-      //}
+            //         Grid.SetRow(rect, rowIndex);
+            //         Grid.SetColumnSpan(rect, 6);
+            //         GridSettings.Children.Add(rect);
+            //      }
+            //   }
+            //}
+        }
 
-      private void DeleteButton_Click(object sender, RoutedEventArgs e)
-      {
-      }
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            _mmodel.Navigate(new Scanner());
+        }
 
-      private void CloseWindow_Click(object sender, RoutedEventArgs e)
-      {
-         _mmodel.Navigate(new Scanner());
-      }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
 
-      private void Save_Click(object sender, RoutedEventArgs e)
-      {
+        }
 
-      }
+        private void btnLoeschen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBearbeiten_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 
 }
---
+++ ViewModel/Converter/NullToBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace RunTrack
+{
+    internal class NullToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
