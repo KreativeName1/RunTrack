822379fde4879471fe0c620b6e7fb5a90ee960f4
fischerp <fischerp@NUCD21021>
2024-12-06 09:46:32 +0100

    Code Kommentriert v1

--- Model/Urkunde.cs
+++ Model/Urkunde.cs
@@ -12,7 +12,7 @@
         public TimeSpan SchnellsteRunde { get; set; }
         public int AnzahlRunden { get; set; }
         public int GelaufeneMeter { get; set; }
-        public string Rundenart { get; set; } // Neue Eigenschaft
+        public string Rundenart { get; set; }
 
         public Urkunde(string laufName, string auswertungsart, string worin, string wert, string platzierung, string name, string geschlecht, TimeSpan schnellsteRunde, int anzahlRunden, int gelaufeneMeter, string rundenart)
         {
