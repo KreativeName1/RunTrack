dc1a6cc2779cc4399857680181c9b6ba4d64372d
Sascha Dierl <dierlsasch05@gmail.com>
2024-12-19 23:54:26 +0100

    Feat(PDF-Editor): Verwalten Button ist nun deaktiviert, wenn keine Formate vorhanden sind

---
+++ ViewModel/Converter/ListNotEmptyConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RunTrack
+{
+    public class ListNotEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IList list)
+            {
+                return list.Count > 0;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("ConvertBack is not implemented.");
+        }
+    }
+}
